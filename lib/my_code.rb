8881339ee06ff4e56fa26a7e131cf8eb9b60684a
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 12:13:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * -1 ) # <== Unique work
+    new.push( yield(source_array[i]) )
     i += 1
   end
   return new
