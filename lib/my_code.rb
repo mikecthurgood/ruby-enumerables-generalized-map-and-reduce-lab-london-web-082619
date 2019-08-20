0ae70916fdc1c1db616ad94dd56d58ceb12b9ad4
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 12:16:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,5 +9,18 @@ def map(source_array)
 end
 
 
-def reduce(source_array)
+def reduce(source_array, start=0)
+	  if start 
+	    i = 0 
+	    total = start
+	  else 
+	    total = source_array[0]
+	    i = 1 
+	  end
+	  while i < source_array.length do
+	    total = yield(total, source_array[i])
+	      i += 1
+	  end 
+	  total
+	end
   
