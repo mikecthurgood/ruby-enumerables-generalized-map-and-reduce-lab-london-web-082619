6715f21078153b69a8243151769fa3aec95c14d1
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 12:14:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( yield(source_array[i]) )
+    new.push(yield(source_array[i]))
     i += 1
   end
   return new
